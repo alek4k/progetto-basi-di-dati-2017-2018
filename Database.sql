97d38b10e70193f882ce9df67e485eb641b03ed6
Alessandro Lovo <alessandro.lovo@gmail.com>
2018-04-27 15:25:03 +0200

    update database.sql

--- Database.sql
+++ Database.sql
@@ -4,16 +4,16 @@ Telefono INTEGER(10) NOT NULL,
 Email VARCHAR(40) NOT NULL,
 Citta VARCHAR(40) NOT NULL,
 Via VARCHAR(50) NOT NULL,
-CAP INTEGER(10) NOT NULL,
+CAP INTEGER(10) NOT NULL
 );
 
-DROP TABLE IF EXISTS Oraio;
+DROP TABLE IF EXISTS Orario;
 CREATE TABLE Orario(
 Giorno VARCHAR(10) NOT NULL,
 Apertura TIME,
 Chiusura TIME,
 Inizio_periodo_chiusura DATE NOT NULL,
-Fine_periodo_chiusura DATE NOT NULL,
+Fine_periodo_chiusura DATE NOT NULL
 );
 
 DROP TABLE IF EXISTS Visitatore;
@@ -26,21 +26,21 @@ DROP TABLE IF EXISTS Biglietto;
 CREATE TABLE Biglietto(
 Id_biglietto VARCHAR(15) PRIMARY KEY,
 Tipo VARCHAR(10) NOT NULL,
-Prezzo DECIMAL(3,2) NOT NULL,
+Prezzo DECIMAL(3,2) NOT NULL
 );
 
 DROP TABLE IF EXISTS Parcheggio;
 CREATE TABLE Parcheggio(
 Id_parcheggio VARCHAR(15) PRIMARY KEY,
 Categoria VARCHAR(10) NOT NULL,
-Prezzo DECIMAL(3,2) NOT NULL,
+Prezzo DECIMAL(3,2) NOT NULL
 );
 
 
 DROP TABLE IF EXISTS TipoDipendenti;
 CREATE TABLE TipoDipendenti(
 Id_dipendente INTEGER PRIMARY KEY,
-Descrizione ENUM('Direttore', 'Keeper', 'Veterinario', 'Cassiere', 'Mascotte', 'Ricercatore') NOT NULL,
+Descrizione ENUM('Direttore', 'Keeper', 'Veterinario', 'Cassiere', 'Mascotte', 'Ricercatore') NOT NULL
 );
 
 DROP TABLE IF EXISTS Dipendenti;
@@ -52,7 +52,7 @@ Citta VARCHAR(50),
 DataAssunzione DATE NOT NULL,
 Salario DECIMAL(4,2) UNSIGNED NOT NULL,
 Tipologia INTEGER,
-FOREIGN KEY(Tipologia) REFERENCES TipoDipendenti(Id_dipendente),
+FOREIGN KEY(Tipologia) REFERENCES TipoDipendenti(Id_dipendente)
 );
 
 DROP TABLE IF EXISTS Area;
@@ -84,7 +84,7 @@ Eta INTEGER(3) NOT NULL,
 Origine VARCHAR(30) NOT NULL,
 Data_arrivo DATE,
 Data_nascita DATE,
-FOREIGN KEY (Id_gabbia) REFERENCES Gabbia(Id_gabbia)
+FOREIGN KEY (Id_gabbia) REFERENCES Gabbia(Id_gabbia),
 FOREIGN KEY (Id_area) REFERENCES Area(Id_area)
 );
 
@@ -97,8 +97,8 @@ FOREIGN KEY (Id_merce) REFERENCES Merce(Id_merce)
 
 DROP TABLE IF EXISTS Merce;
 CREATE TABLE Merce(
-Id_merce VARCHAR(15) PRIMARY_KEY,
-Prezzo DECIMAL(2,2) NOT NULL,
+Id_merce VARCHAR(15) PRIMARY KEY,
+Prezzo DECIMAL(2,2) NOT NULL
 );
 
 
